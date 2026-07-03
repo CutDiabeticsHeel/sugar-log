394f03960dd82ebb802e2e7f288ff142bdd1fbad
shishioZXC <niksizdog@gmail.com>
2026-07-03 17:38:02 +0300

    build: creat app with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  // Инициализируем состояние счетчика
+  const [count, setCount] = useState(1);
+
+  // Функция увеличения значения
+  function handleClick() {
+    setCount(count * 2);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Счетчик: {count}</h1>
+      <button onClick={handleClick}>Увеличить</button>
     </div>
   );
 }
