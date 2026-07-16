95b6a52d7096ac3710558b87acc5ebf8cb4607a2
shishioZXC <niksizdog@gmail.com>
2026-07-16 15:00:18 +0300

    feat: add menu function

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,21 +1,56 @@
 import { Link } from "react-router-dom";
-import style from "../css/menu.module.css";
+import style from "../css/components/menu.module.css";
 import WaterDropIcon from '@mui/icons-material/WaterDrop';
 import ListAltIcon from '@mui/icons-material/ListAlt';
 import ShowChartIcon from '@mui/icons-material/ShowChart';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
 import PersonIcon from '@mui/icons-material/Person';
+import MenuIcon from '@mui/icons-material/Menu';
+import {useState} from "react";
 
 function Menu() {
+    const [isOpen, setOpen] = useState(true)
+
     return (
-        <nav className={style.navigationMenu}>
-            <Link to="/" className={style.navigationItem}><WaterDropIcon/>Главная <span>01</span></Link>
-            <Link to="/diary" className={style.navigationItem}><ListAltIcon/>Дневник <span>02</span></Link>
-            <Link to="/graph" className={style.navigationItem}><ShowChartIcon/>Графики <span>03</span></Link>
-            <Link to="/statistics" className={style.navigationItem}><BarChartIcon/>Статистика <span>04</span></Link>
-            <Link to="/products" className={style.navigationItem}><RestaurantMenuIcon/>Продукты <span>05</span></Link>
-            <Link to="/profile" className={style.navigationItem}><PersonIcon/>Профиль <span>06</span></Link>
+        <nav className={`${style.navigationMenu} ${isOpen ? style.openMenu : style.closeMenu}`}>
+            <span className={style.titleMenu}>
+                <span className={style.linkText}>Дневник <br/> сахаров</span>
+                <button className={style.buttonMenu} onClick={() => (setOpen(prev => !prev))}>
+                    <MenuIcon/>
+                </button>
+            </span>
+
+            <Link to="/" className={style.navigationItem}>
+                <WaterDropIcon/>
+                <span className={style.linkText}>Главная</span>
+                <span className={style.linkNumber}>01</span>
+            </Link>
+            <Link to="/diary" className={style.navigationItem}>
+                <ListAltIcon/>
+                <span className={style.linkText}>Дневник</span>
+                <span className={style.linkNumber}>02</span>
+            </Link>
+            <Link to="/graph" className={style.navigationItem}>
+                <ShowChartIcon/>
+                <span className={style.linkText}>Графики</span>
+                <span className={style.linkNumber}>03</span>
+            </Link>
+            <Link to="/statistics" className={style.navigationItem}>
+                <BarChartIcon/>
+                <span className={style.linkText}>Статистика</span>
+                <span className={style.linkNumber}>04</span>
+            </Link>
+            <Link to="/products" className={style.navigationItem}>
+                <RestaurantMenuIcon/>
+                <span className={style.linkText}>Продукты</span>
+                <span className={style.linkNumber}>05</span>
+            </Link>
+            <Link to="/profile" className={style.navigationItem}>
+                <PersonIcon/>
+                <span className={style.linkText}>Профиль</span>
+                <span className={style.linkNumber}>06</span>
+            </Link>
         </nav>
     )
 }
